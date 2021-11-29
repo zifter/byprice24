69406b3f2675f633943c8a7be003d7f08adb244a
anton <anton@wsa-173-72a.icx.local>
2021-11-29 20:21:35 +0300

    Add endpoint to search products

---
+++ src/marketplace/filters.py
@@ -0,0 +1,10 @@
+from marketplace.serializers import ProductSearchSerializer
+from rest_framework import filters
+
+
+class CustomSearchFilter(filters.SearchFilter):
+    search_param = 'query'
+
+    def filter_queryset(self, request, queryset, view):
+        ProductSearchSerializer(data=request.query_params).is_valid(raise_exception=True)
+        return super().filter_queryset(request, queryset, view)
