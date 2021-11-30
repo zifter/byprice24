350a0354cb099c7261d8835b01056505367f3edb
Anton Kuzmich <71846593+Anthonykuzmich@users.noreply.github.com>
2021-11-30 22:50:28 +0300

    Add product search  (#38)
    * Add endpoint to search products

--- src/marketplace/urls.py
+++ src/marketplace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import MarketplaceViewSet
+from .views import ProductViewSet
 
 marketplace_list = MarketplaceViewSet.as_view({
     'get': 'list',
@@ -9,8 +10,12 @@ marketplace_detail = MarketplaceViewSet.as_view({
     'get': 'retrieve',
 })
 
+product_search_list = ProductViewSet.as_view({
+    'get': 'list'
+})
 
 urlpatterns = [
     path('v1/marketplaces/', marketplace_list, name='marketplace-list'),
     path('v1/marketplaces/<str:domain>', marketplace_detail, name='marketplace-detail'),
+    path('v1/search/products', product_search_list, name='product-search-list'),
 ]
--- src/marketplace/views.py
+++ src/marketplace/views.py
@@ -1,5 +1,8 @@
+from marketplace.filters import CustomSearchFilter
 from marketplace.models import Marketplace
+from marketplace.models import Product
 from marketplace.serializers import MarketplaceSerializer
+from marketplace.serializers import ProductSerializer
 from rest_framework import viewsets
 
 
@@ -11,5 +14,16 @@ class MarketplaceViewSet(viewsets.ModelViewSet):
     queryset = Marketplace.objects
     serializer_class = MarketplaceSerializer
     lookup_field = 'domain'
-    ordering_fields = ('domain', )
+    ordering_fields = ('domain',)
     ordering = ('domain',)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API Product
+    """
+    model = Product
+    queryset = Product.objects
+    serializer_class = ProductSerializer
+    filter_backends = [CustomSearchFilter]
+    search_fields = ['name']
