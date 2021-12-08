f7c9939b07b70358d1de54cd0ee352bae8b450e6
zifter <zifter.ai@gmail.com>
2021-12-09 02:56:33 +0300

    wip

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -25,12 +25,12 @@ class Base(Configuration):
     # TODO Override secret in production
     SECRET_KEY = os.environ.setdefault('SECRET_KEY', 'django-insecure-tfv^rapkl5+j5&c+x64-iy3#m+hpmhyj10f^b(ww2xxu&_#78+')
 
-    ALLOWED_HOSTS = [
+    ALLOWED_HOSTS = [{
+        '0.0.0.0',
         os.environ.setdefault('POD_IP', '127.0.0.1'),
         os.environ.setdefault('DOMAIN_API', 'localhost'),
         os.environ.setdefault('DOMAIN_ADMIN', 'localhost'),
-        '0.0.0.0',
-    ]
+    }]
 
     # Application definition
 
