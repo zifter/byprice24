726b6605defd445e71f838a2e9cfba83b74a45a7
Anton Kuzmich <71846593+Anthonykuzmich@users.noreply.github.com>
2021-12-09 22:58:17 +0300

    Improved search (#71)
    Co-authored-by: anton <anton@wsa-173-72a.icx.local>

--- src/marketplace/serializers.py
+++ src/marketplace/serializers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from marketplace.models import Marketplace
 from marketplace.models import Product
 from rest_framework import serializers
@@ -9,11 +11,29 @@ class MarketplaceSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
+    def to_representation(self, instance):
+        product_offers = self.get_offers(instance)
+        return {'marketplaces_count_instock': len(instance.product_pages),
+                'min_offer': min(product_offers, key=lambda x: x['price']),
+                **super().to_representation(instance)}
 
-class ProductSearchSerializer(serializers.Serializer):
+    @staticmethod
+    def get_offers(instance) -> List[dict, ]:
+        price_offers = []
+        product_pages = instance.product_pages
+
+        for page in product_pages:
+            product_state = max(page.product_state, key=lambda x: x.created)
+            price_offers.append(dict(price=float(product_state.price),
+                                     price_currency=product_state.price_currency))
+
+        return price_offers
+
+
+class ProductQuerySerializer(serializers.Serializer):
     query = serializers.CharField(min_length=3)
