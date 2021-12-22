b5f6eaeeae152cbaec1f033f2ccc8392b5859781
Anton Kuzmich <71846593+Anthonykuzmich@users.noreply.github.com>
2021-12-22 18:31:52 +0300

    Add google analytics to app (#92)
    * Add google analytics to app.js
    
    * Fixed tests
    
    * Add tracking id to app.js and removed from .env
    
    * Add react.ga package to npm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import SearchBar from './pages/components/SearchBar.js';
 import Home from './pages/Home.js';
 import Search from './pages/Search.js';
 import Product from './pages/Product.js';
+import ReactGA from 'react-ga';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-R18TXE65QV',
+        {testMode: process.env.NODE_ENV === 'test'});
+    // To Report Page View
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  useEffect(() => {
+    console.log(window.location.pathname);
+  });
   return (
     <div>
       <BrowserRouter>
