7e58b144228cb99265f828e24203695b0d5a4275
Anton Kuzmich <71846593+Anthonykuzmich@users.noreply.github.com>
2021-12-28 10:51:33 +0300

    Add scraper for funtasticby (#102)
    * Add funtastic scraper logics
    
    * Refactor folders and files -> created folder for structured data classes
    
    * Add migrations
    
    * Returned the structure
    
    * Shorten the description by paragrapths if the decsription is more than 512 symbols
    
    * Fix comments
    
    * Refactor
    
    * Fix bug with none product page for funtastic

---
+++ src/scraper/spiders/funtasticby.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from common.item_types import Category
+from scraper.base import CategoryRule
+from scraper.base import SpiderBase
+from scraper.items import ProductScrapingResult
+from scraper.mixin import StructuredDataMixin
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
+
+
+class Spider(SpiderBase, StructuredDataMixin):
+    name: str = 'www.funtastik.by'
+    allowed_domains = [
+        'www.funtastik.by',
+    ]
+
+    rules = (
+
+        CategoryRule(LinkExtractor(allow=('igrushki/konstruktory',)), category=Category.LEGO),
+    )
+
+    def parse_product_impl(self, response: Response, category: Category) -> Optional[ProductScrapingResult]:
+        structured_data = self.extract_structured_data(response, category)
+        if structured_data:
+            structured_data.title = structured_data.title.split(' Посмотреть')[0]
+        return structured_data
