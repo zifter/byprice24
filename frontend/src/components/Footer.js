475e8a9362ac4d8c5bab027967d8e2fa6dd3771f
Kirill <3331668@mail.ru>
2022-03-04 17:45:49 +0300

    fix lint (#134)

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,58 +5,58 @@ import {Text, Container,
 
 const Footer = () => {
   return (
-      <Container
-          pos="relative"
-          h="100%"
-          bottom="0">
-        <Row>
+    <Container
+      pos="relative"
+      h="100%"
+      bottom="0">
+      <Row>
 
-          <Col size={2}>
+        <Col size={2}>
+          <Text
+            textSize="body"
+            pos="relative"
+            p="0"
+            textAlign="center">
+            2021 by zifter
+
+          </Text>
+        </Col>
+        <Col size={5}>
+          <Anchor
+            href="/disclaimer"
+            d="block"
+            m={{b: '1rem'}}
+          >
             <Text
-                textSize="body"
-                pos="relative"
-                p="0"
-                textAlign="center">
-              2021 by zifter
-
+              textSize="body"
+              pos="relative"
+              p="0"
+              textAlign="left"
+              textColor="black">
+              Правовая информация.
             </Text>
-          </Col>
-          <Col size={5}>
-            <Anchor
-                href="/disclaimer"
-                d="block"
-                m={{b: '1rem'}}
-            >
-              <Text
-                  textSize="body"
-                  pos="relative"
-                  p="0"
-                  textAlign="left"
-                  textColor="black">
-                Правовая информация.
-              </Text>
-            </Anchor>
-          </Col>
-          <Col>
-            <Anchor
-                href="/contacts"
-                d="block"
-                m={{b: '1rem'}}
-            >
-              <Text
-                  textSize="body"
-                  pos="relative"
-                  p="0"
-                  textAlign="left"
-                  textColor="black">
-                Свяжитесь с нами:
-                контакты
-              </Text>
-            </Anchor>
-          </Col>
-        </Row>
+          </Anchor>
+        </Col>
+        <Col>
+          <Anchor
+            href="/contacts"
+            d="block"
+            m={{b: '1rem'}}
+          >
+            <Text
+              textSize="body"
+              pos="relative"
+              p="0"
+              textAlign="left"
+              textColor="black">
+              Свяжитесь с нами:
+              контакты
+            </Text>
+          </Anchor>
+        </Col>
+      </Row>
 
-      </Container>
+    </Container>
   );
 };
 
