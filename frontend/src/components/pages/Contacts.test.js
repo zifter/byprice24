233e23cd6edfeaa3d8ec519efc1da0207a869173
Kirill <3331668@mail.ru>
2022-03-05 17:51:20 +0300

    fix lint in test for Contacts (#134)

--- frontend/src/components/pages/Contacts.test.js
+++ frontend/src/components/pages/Contacts.test.js
@@ -3,13 +3,14 @@ import {render, screen} from '@testing-library/react';
 import Contacts from './Contacts';
 
 test('renders Contact info', () => {
-    render(<Contacts/>);
-    const textElement = screen.getByText('Контакты');
-    expect(textElement).toBeInTheDocument();
-
+  render(<Contacts/>);
+  const textElement = screen.getByText('Контакты');
+  expect(textElement).toBeInTheDocument();
 });
 
 test('should navigate to ... when link is clicked', () => {
-    const { getByTestId } = render(<a data-testid='link' href="mailto:contact@findprice.by">Click Me</a>);
-    expect(getByTestId('link')).toHaveAttribute('href', 'mailto:contact@findprice.by');
+  const {getByTestId} = render(<a data-testid='link'
+    href="mailto:contact@findprice.by">Click Me</a>);
+  expect(getByTestId('link')).toHaveAttribute('href',
+   'mailto:contact@findprice.by');
 });
