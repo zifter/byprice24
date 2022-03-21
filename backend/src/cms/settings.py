53c576d5ee70042572fec5bcb0c4c8784288f053
ElenaVolchkova <sombradzi@gmail.com>
2022-03-21 15:00:22 +0300

    Store List API

--- backend/src/cms/settings.py
+++ backend/src/cms/settings.py
@@ -58,6 +58,7 @@ class Base(Configuration):
 
     REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+        'PAGE_SIZE': 15
     }
 
     MIDDLEWARE = [
--- backend/src/marketplace/urls.py
+++ backend/src/marketplace/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include
 from django.urls import path
-from marketplace.views import ProductDetailsViewSet
+from marketplace.views import ProductDetailsViewSet, MarketplacesViewSet, MarketplaceDetailsViewSet
 from search.views import SearchProductViewSet
 
 
@@ -9,4 +9,6 @@ urlpatterns = [
 
     path('v1/search/products', SearchProductViewSet.as_view(), name='product-search-list'),
     path('v1/products/<str:id>', ProductDetailsViewSet.as_view(), name='product-detail'),
+    path('v1/marketplaces', MarketplacesViewSet.as_view()),
+    path('v1/marketplaces/<str:pk>', MarketplaceDetailsViewSet.as_view()),
 ]
